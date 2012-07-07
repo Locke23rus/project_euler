2e6b03f2441070484c7513a36bd0e7e429948fa7
Kirill Nikitin <locke23rus@gmail.com>
2012-07-07 16:46:43 +0400

    Update 9.rb

--- 9.rb
+++ 9.rb
@@ -1,25 +1,61 @@
-limit = 500
-sum_equal = false
-product_equal = false
-product = 0
+require 'benchmark'
 
-100.upto limit do |a|
-  (a + 1).upto limit do |b|
-    (b + 1).upto limit do |c|
-      sum = a + b + c
-      break if sum > 1000
-      sum_equal = sum == 1000
-      if sum_equal
-        product_equal = a**2 + b**2 == c**2
-        if product_equal
-          product = a * b * c
-          break
-        end
+def lame
+  limit = 500
+  100.upto limit do |a|
+    (a + 1).upto limit do |b|
+      (b + 1).upto limit do |c|
+        sum = a + b + c
+        break if sum > 1000
+        return a*b*c if sum == 1000 && c**2 == a**2 + b**2
       end
     end
-    break if sum_equal && product_equal
   end
-  break if sum_equal && product_equal
 end
 
-puts product
+def pro_1
+  s = 1000
+  3.upto (s-3) / 3 do |a|
+    (a+1).upto (s - 1 - a) / 2 do |b|
+      c = s - a - b
+      return a*b*c if c**2 == a**2 + b**2
+    end
+  end
+end
+
+def pro_2
+  s = 1000
+  s2 = s / 2
+  mlimit = Math.sqrt(s2).ceil - 1
+  2.upto mlimit do |m|
+    if s2 % m == 0
+      sm = s2 / m
+      while sm % 2 == 0
+        sm = sm / 2
+      end
+      if m % 2 == 1
+        k = m + 2
+      else
+        k = m + 1
+      end
+      while k < 2*m && k <= sm
+        if sm % k == 0 && k.gcd(m) == 1
+          d = s2 / (k*m)
+          n = k - m
+          a = d * (m**2 - n**2)
+          b = 2 * d * m * n
+          c = d *(m**2 + n**2)
+          return a*b*c
+        end
+        k += 2
+      end
+    end
+  end
+end
+
+n = 10
+Benchmark.bm do |x|
+  x.report { n.times { lame } }
+  x.report { n.times { pro_1 } }
+  x.report { n.times { pro_2 } }
+end
